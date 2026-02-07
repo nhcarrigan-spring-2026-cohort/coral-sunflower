584e7fab16922396d20d16a6e0c9cebc4c9e456e
rashiqazhan <rashiqazhan@gmail.com>
2026-02-07 06:59:55 +0600

    feat: Add shared packages example.

--- apps/backend/src/server/main.ts
+++ apps/backend/src/server/main.ts
@@ -1,9 +1,12 @@
 import express from "express";
+import {type Hello} from "@repo/types/response";
+import {HELLO} from "@repo/constants/response";
 
 const app = express();
 
 app.get("/", (_, res) => {
-  res.send("Hello coral-sunflower!");
+  const response: Hello = HELLO;
+  res.send(response);
 });
 
 app.listen(3000, () => console.log("Server is listening on port 3000..."));

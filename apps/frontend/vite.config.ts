abc534519772d2f6f37ccf4a607ef2b09f524bf7
rashiqazhan <rashiqazhan@gmail.com>
2026-02-13 23:18:57 +0600

    feat: Add frontend dockerfile.

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -4,4 +4,11 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    watch: {
+      ignored: ["**/node_modules/**"],
+      usePolling: true,
+      interval: 1000,
+    },
+  },
 });

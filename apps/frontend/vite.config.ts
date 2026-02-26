017ad9efc887be1a7485f0318f697b2707ce0395
rashiqazhan <rashiqazhan@gmail.com>
2026-02-26 21:42:37 +0600

    feat: Add tailwind and shadcn.

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,15 +1,22 @@
+import path from "node:path";
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tailwindcss(), tsconfigPaths()],
   server: {
     watch: {
       ignored: ["**/node_modules/**"],
       usePolling: true,
       interval: 1000,
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
     },
   },
 });

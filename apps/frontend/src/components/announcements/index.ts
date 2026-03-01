6e53d533bc3a42e40841c8efbf0a9d1098d77a93
Rajkoli143 <2024.rajk@isu.ac.in>
2026-03-01 21:42:18 +0530

    refactor: remove barrel imports in favor of direct imports

--- apps/frontend/src/components/announcements/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as AnnouncementForm } from "./AnnouncementForm";
-export { default as AnnouncementList } from "./AnnouncementList";
-export { default as AnnouncementSidebar } from "./AnnouncementSidebar";

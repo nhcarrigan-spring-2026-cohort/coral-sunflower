acd016c51b681543cfd415b3d16e00a6a6d351be
Rajkoli143 <2024.rajk@isu.ac.in>
2026-03-01 21:40:31 +0530

    refactor: move announcements from features/ to components/ to reduce nesting

---
+++ apps/frontend/src/components/announcements/index.ts
@@ -0,0 +1,3 @@
+export { default as AnnouncementForm } from "./AnnouncementForm";
+export { default as AnnouncementList } from "./AnnouncementList";
+export { default as AnnouncementSidebar } from "./AnnouncementSidebar";
--- apps/frontend/src/features/announcements/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as AnnouncementForm } from "./components/AnnouncementForm";
-export { default as AnnouncementList } from "./components/AnnouncementList";
-export { default as AnnouncementSidebar } from "./components/AnnouncementSidebar";
